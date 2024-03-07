eac2c5ed20bdebc30ca3b84602a5feb437fa34aa
syed7447 <162137022+syed7447@users.noreply.github.com>
2024-03-07 14:53:39 +0530

    changes

--- src/main/java/com/example/demo/controller/SpecializationController.java
+++ src/main/java/com/example/demo/controller/SpecializationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Specialization;
 import com.example.demo.service.ISpecializationService;
@@ -39,6 +40,12 @@ public class SpecializationController {
 		
 		return "SpecializationData";
 		
+	}
+	@GetMapping("/delete")
+	public String deleteData(@RequestParam Long id) {
+		service.removeSpecialization(id);
+		
+		return "redirect:all";
 	}
 
 }
